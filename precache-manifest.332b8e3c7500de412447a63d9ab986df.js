8ce12ded342082d4cf461bbd8e522291dcdfd305
youvovk <wer3wollf@gmail.com>
2019-11-29 22:07:48 +0200

    Updates

---
+++ precache-manifest.332b8e3c7500de412447a63d9ab986df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a39c5b98d09a13b351f9067ac13cbe1",
+    "url": "/mobila_app-landing-page/index.html"
+  },
+  {
+    "revision": "db9bfc274a288f48774c",
+    "url": "/mobila_app-landing-page/static/css/main.f7f61ce9.chunk.css"
+  },
+  {
+    "revision": "43b0111713d9ee449cbf",
+    "url": "/mobila_app-landing-page/static/js/2.2d8a1092.chunk.js"
+  },
+  {
+    "revision": "db9bfc274a288f48774c",
+    "url": "/mobila_app-landing-page/static/js/main.4dd1e1a0.chunk.js"
+  },
+  {
+    "revision": "2ddf949fafef9e5396d8",
+    "url": "/mobila_app-landing-page/static/js/runtime-main.df478235.js"
+  },
+  {
+    "revision": "b3be96cbbfd0ff1f3ba3c7c2f58c1441",
+    "url": "/mobila_app-landing-page/static/media/Roboto-Regular-webfont.b3be96cb.woff"
+  },
+  {
+    "revision": "657e8f6754fde1201c6cff374bea06cc",
+    "url": "/mobila_app-landing-page/static/media/blue-features.657e8f67.svg"
+  },
+  {
+    "revision": "528a2d63d1d471bd880f45ae2a814e7e",
+    "url": "/mobila_app-landing-page/static/media/desktop.528a2d63.jpg"
+  },
+  {
+    "revision": "3a8a3f36cfc70432ab1ca1f2a5e5bad2",
+    "url": "/mobila_app-landing-page/static/media/footer_phones.3a8a3f36.svg"
+  },
+  {
+    "revision": "5b4ebb68af85e53c8b1b083467a417fa",
+    "url": "/mobila_app-landing-page/static/media/mobile-link.5b4ebb68.svg"
+  },
+  {
+    "revision": "2ece6332376b4d7a1d40264d83cdfffb",
+    "url": "/mobila_app-landing-page/static/media/notif.2ece6332.svg"
+  },
+  {
+    "revision": "c2b53ad5a6c9f62f8e54c998994cf67b",
+    "url": "/mobila_app-landing-page/static/media/react-mob.c2b53ad5.png"
+  },
+  {
+    "revision": "9b85779913700d94956a7ca6ae811ac1",
+    "url": "/mobila_app-landing-page/static/media/start-play.9b857799.svg"
+  },
+  {
+    "revision": "98e532297ca924c910870a68936a8653",
+    "url": "/mobila_app-landing-page/static/media/what.98e53229.svg"
+  }
+]);
